89b8eb6e0e1aed1324db61f07c4b290c968a91f8
Chin <31579962+iamnichchin@users.noreply.github.com>
2025-04-22 04:10:54 +0800

    added configs for testing env named assessment

---
+++ config/assessment.js
@@ -0,0 +1,8 @@
+// config/dev.js
+module.exports = {
+    baseURL: 'https://auth.wingz.me/auth/signin',
+    credentials: {
+        email: 'chin-mail@mailinator.com',
+        password: 'Nich102298!'
+      }
+};
