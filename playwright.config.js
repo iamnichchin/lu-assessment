df90ae752ec09ee17562b0b4d706958b61685aba
Chin <31579962+iamnichchin@users.noreply.github.com>
2025-04-22 06:28:49 +0800

    headless added true value

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     timeout: 30000,
-    headless: false,
+    headless: true,
   },
 
   /* Configure projects for major browsers */
